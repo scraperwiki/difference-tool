93b867278a9e3699a3b51b7f850b69ab390e15cf
Steven Maude <steve@scraperwiki.com>
2015-04-15 13:44:23 +0000

    Make HTML from aha word wrapped

--- diff_text.sh
+++ diff_text.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-dwdiff -i -c -A best text_from_url1 text_from_url2 | aha > /home/tool/http/diff.html
+dwdiff -i -c -A best text_from_url1 text_from_url2 | aha -w > /home/tool/http/diff.html
